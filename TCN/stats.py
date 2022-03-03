62c4934e14668db460894533f7331e0d00c15999
kch4fk <kch4fk@virginia.edu>
2022-03-03 16:17:08 -0500

    calculate stats on gesture/MP duration

---
+++ TCN/stats.py
@@ -0,0 +1,183 @@
+# Kay Hutchinson 2/10/2022
+#
+# Get stats on the durations of gestures, surgemes, and motion primitives
+
+
+# Imports
+import os
+import sys
+import glob
+import shutil
+import csv
+from csv import reader, writer
+import numpy as np
+import pandas as pd
+import cv2
+from matplotlib import pyplot as plt
+from preprocess import processArguments
+
+
+baseDir = os.path.dirname(os.getcwd())
+sysDir = os.path.join(baseDir, "Datasets", "dV")
+allTasks = ["Suturing", "Needle_Passing", "Knot_Tying", "Peg_Transfer", "Pea_on_a_Peg", "Post_and_Sleeve"]
+labeltypes = ["motion_primitives", "gestures"]
+
+
+
+# Process arguments from command line
+# returns set and labeltype
+# def processArguments(args):
+#     # Get arguments from command line
+#     try:
+#         set=args[1]
+#         # Check if valid set
+#         if set not in ["DESK", "JIGSAWS", "ROSMA", "All"]:
+#             print("Please choose set: DESK, JIGSAWS, ROSMA, All")
+#             sys.exit()
+#     except:
+#         print("Please choose set: DESK, JIGSAWS, ROSMA All")
+#         sys.exit()
+#
+#     # Get MP or gesture from command line
+#     try:
+#         labeltype = args[2]
+#         # Check if valid labeltype
+#         if labeltype not in ["motion_primitives", "gestures"]:
+#             print("Please choose label type: motion_primitives gestures")
+#             sys.exit()
+#     except:
+#         print("Please choose label type: motion_primitives gestures")
+#         sys.exit()
+#
+#     # return arguments
+#     return set, labeltype
+
+
+# For a given label type, look at all label transcripts and calculate mean and
+# stdev of each label type
+def getStats(tasks, labeltype, durations, instances):
+    # For each task
+    for task in tasks:
+        print(task)
+        taskDir = os.path.join(sysDir, task, labeltype)
+        # For each label transcript
+        for file in os.listdir(taskDir):
+            fileIn = os.path.join(taskDir, file)
+            # Read in label transcript
+            if labeltype == "motion_primitives":
+                tg = pd.read_table(fileIn) #, sep="\s")
+            elif labeltype == "gestures":
+                tg = pd.read_csv(fileIn, header=None)
+            #print(tg)
+
+            # For each line, get start and end frames and class label
+            for i in range(tg.shape[0]):
+                line = tg.iloc[i,0].replace("\t", " ")
+                line = line.split()
+
+                start = int(line[0])
+                end = int(line[1])
+                labels = ''.join(line[2:])
+                label = labels.split("(")[0]
+                # Calculate duration and update durations and instances dictionaries
+                dur = end - start
+                #durations[label] = durations[label].append(dur)
+                durations[label].append(dur)
+                instances[label] = instances[label] + 1
+
+    #print(durations)
+    #print(instances)
+
+    # Calculate stats
+    for i in durations:
+        if instances[i] != 0:
+            print(i)
+            print("Number of examples: " + str(len(durations[i])))
+            print("Average Duration: " + str(sum(durations[i])/len(durations[i])))
+            print("Standard Deviation: " + str(np.std(durations[i])))
+            print("Max Duration: " + str(max(durations[i])))
+            print("Min Duration: " + str(min(durations[i])))
+            print("\n")
+            #print(i + " " + str(durations[i]/instances[i]))
+
+            # Plot durations as a histogram
+            # plt.hist(durations[i])
+            # plt.title("Durations of " + i + " in " + set)
+            # plt.xlabel("Duration (# samples)")
+            # plt.ylabel("Number of instances")
+            # plt.show()
+
+
+
+
+
+# Main
+if __name__ == "__main__":
+    global set
+    # Process arugments from command line and get set, var, and labeltype
+    set, var, labeltype, valtype = processArguments(sys.argv)
+    if labeltype == "MP":
+        labeltype = "motion_primitives"
+    elif labeltype == "gesture":
+        labeltype = "gestures"
+
+    # Choose which set of tasks to analyze based on set
+    if set == "All":
+        tasks = allTasks
+    elif set == "JIGSAWS":
+        tasks = allTasks[0:3]
+    elif set == "DESK":
+        tasks = [allTasks[3]]
+    elif set == "ROSMA":
+        tasks = allTasks[4:6]
+    elif (set == "All-5a") or (set == "All-5b"):
+        tasks = allTasks[0:4]
+
+    elif set == "S":
+        tasks = [allTasks[0]]
+    elif set == "NP":
+        tasks = [allTasks[1]]
+    elif set == "KT":
+        tasks = [allTasks[2]]
+    elif set == "PoaP":
+        tasks = [allTasks[4]]
+    elif set == "PaS":
+        tasks = [allTasks[5]]
+    #print(tasks)
+
+    # Dictionaries to store number of instances of label class and total duration
+    durationsG = {"G1": [], "G2": [], "G3": [], "G4": [], "G5": [], "G6": [], "G7": [], "G8": [], "G9": [], "G10": [], "G11": [], "G12": [], "G13": [], "G14": [], "G15": []}
+    durationsS = {"S1": [], "S2": [], "S3": [], "S4": [], "S5": [], "S6": [], "S7": []}
+    durationsMP = {"Grasp": [], "Release": [], "Touch": [], "Untouch": [], "Pull": [], "Push": []}
+    instancesG = {"G1": 0, "G2": 0, "G3": 0, "G4": 0, "G5": 0, "G6": 0, "G7": 0, "G8": 0, "G9": 0, "G10": 0, "G11": 0, "G12": 0, "G13": 0, "G14": 0, "G15": 0}
+    instancesS = {"S1": 0, "S2": 0, "S3": 0, "S4": 0, "S5": 0, "S6": 0, "S7": 0}
+    instancesMP = {"Grasp": 0, "Release": 0, "Touch": 0, "Untouch": 0, "Pull": 0, "Push": 0}
+
+    # Choose which set of dictionaries to use based on set and labeltype
+    if labeltype == "motion_primitives":
+        durations = durationsMP
+        instances = instancesMP
+    elif (labeltype == "gestures") and (set == "JIGSAWS"):
+        durations = durationsG
+        instances = instancesG
+    elif (labeltype == "gestures") and (set == "S"):
+        durations = durationsG
+        instances = instancesG
+    elif (labeltype == "gestures") and (set == "NP"):
+        durations = durationsG
+        instances = instancesG
+    elif (labeltype == "gestures") and (set == "KT"):
+        durations = durationsG
+        instances = instancesG
+    elif (labeltype == "gestures") and (set == "DESK"):
+        durations = durationsS
+        instances = instancesS
+    else:
+        print("Invalid combination of arguments. Please note ROSMA doesn't have gesture labels.")
+        sys.exit()
+
+    getStats(tasks, labeltype, durations, instances)
+    print("Done!")
+
+
+# EOF
